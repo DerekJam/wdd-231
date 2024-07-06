ba8560fa040b03d29a3dfa7c03af218fe7911d6b
DerekJam <steeled@byui.edu>
2024-07-06 10:09:00 -0700

    Update script.js

--- chamber/data/script.js
+++ chamber/data/script.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     fetch('chamber/data.json')
         .then(response => response.json())
-        .then(data => {
-            const dataDisplay = document.getElementById("dataDisplay");
-
-           
-        })
+        
         .catch(error => console.error("Error fetching JSON data:", error));
 });
