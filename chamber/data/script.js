a59b4f607618f82ffd33825f1d083b46fb0955b1
DerekJam <steeled@byui.edu>
2024-07-06 09:40:55 -0700

    update

--- chamber/data/script.js
+++ chamber/data/script.js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('data/members.json')
+    fetch('data.json')
         .then(response => response.json())
         .then(data => {
             const dataDisplay = document.getElementById("dataDisplay");
 
             // Create HTML elements to display the JSON data
             const nameElement = document.createElement("p");
-            nameElement.textContent = "Name: " + members.name;
+            nameElement.textContent = "Name: " + data.name;
 
             const addressElement = document.createElement("p");
-            addressElement.textContent = "Address: " + members.address;
+            addressElement.textContent = "Address: " + data.address;
 
             const phoneElement = document.createElement("p");
-            phoneElement.textContent = "Phone: " + members.phone;
+            phoneElement.textContent = "Phone: " + data.phone;
 
             const websiteElement = document.createElement("p");
-            websiteElement.textContent = "Website: " + members.website;
+            websiteElement.textContent = "Website: " + data.website;
 
             // Append the elements to the "dataDisplay" div
-            membersDisplay.appendChild(nameElement);
-            membersDisplay.appendChild(addressElement);
-            membersDisplay.appendChild(phoneElement);
-            membersDisplay.appendChild(websiteElement);
+            dataDisplay.appendChild(nameElement);
+            dataDisplay.appendChild(addressElement);
+            dataDisplay.appendChild(phoneElement);
+            dataDisplay.appendChild(websiteElement);
         })
         .catch(error => console.error("Error fetching JSON data:", error));
 });
